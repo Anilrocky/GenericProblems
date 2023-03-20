458ab9aa7a41548cbfc06f039e5bb5cf4e3b39a1
Anilrocky <anilkumarac96@gmail.com>
2023-03-20 20:01:39 +0530

    [Anil] Add:Folder Structure Created

---
+++ GenericProblems/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace GenericProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generic problem");
+        }
+    }
+}
